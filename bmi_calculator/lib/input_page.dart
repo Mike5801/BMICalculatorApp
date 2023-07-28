0c74e3fe48b812f9f926d3218a9683b205dd2afc
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-07-27 19:35:26 -0600

    add icon text component

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const Color defaultCardColor = Color(0xFF1D1E33);
 const Color bottomContainerColor = Color(0xFFEB1555);
+const Color iconTextColor = Color(0xFF8D8E98);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -26,25 +28,32 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: Card(defaultCardColor),
+                  child: Card(
+                    color: defaultCardColor,
+                    cardChild:
+                        IconText(text: 'MALE', icon: FontAwesomeIcons.mars),
+                  ),
                 ),
                 Expanded(
-                  child: Card(defaultCardColor),
+                  child: Card(
+                    color: defaultCardColor,
+                    cardChild: IconText(text: 'FEMALE', icon: FontAwesomeIcons.venus),
+                  ),
                 ),
               ],
             ),
           ),
           const Expanded(
-            child: Card(defaultCardColor),
+            child: Card(color: defaultCardColor),
           ),
           const Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Card(defaultCardColor),
+                  child: Card(color: defaultCardColor),
                 ),
                 Expanded(
-                  child: Card(defaultCardColor),
+                  child: Card(color: defaultCardColor),
                 ),
               ],
             ),
@@ -61,17 +70,52 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class Card extends StatelessWidget {
-  final Color color;
+class IconText extends StatelessWidget {
 
-  const Card(this.color, {super.key});
+  const IconText({required this.text, required this.icon, super.key});
+
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        const SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: const TextStyle(
+            fontSize: 18.0,
+            color: iconTextColor,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({required this.color, this.cardChild, super.key});
+
+  final Color color;
+  final Widget? cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-          color: color, borderRadius: BorderRadius.circular(10.0)),
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: cardChild,
     );
   }
 }
